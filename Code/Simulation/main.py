61a15afc1ec7768b0737cca8bdde3ef88a7b2cd4
dlleyda <57436913+dlleyda@users.noreply.github.com>
2023-03-04 16:35:04 +0100

    Añadir verbose = False para quitar prints() innecesarios

--- Code/Simulation/main.py
+++ Code/Simulation/main.py
@@ -16,4 +16,10 @@ server = mesa.visualization.ModularServer(
     Ecosistem, [grid], "Ecosistem", {"N": 100, "width": 10, "height": 10}
 )
 server.port = 8522  # The default
+server.verbose = False
 server.launch()
+
+model = Ecosistem(3, 2, 2)
+for i in range(70):
+    # print("Hola")
+    model.step()
